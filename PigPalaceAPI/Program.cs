a71228afeef20c6623e81b784c36355eca16cf78
21522354 <120326395+21522354@users.noreply.github.com>
2024-03-29 20:22:53 +0700

    Configure JWT Token

--- PigPalaceAPI/Program.cs
+++ PigPalaceAPI/Program.cs
@@ -1,8 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PigPalaceAPI.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region configure Services
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -11,7 +15,30 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<PigPalaceDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PigPalaceDB")));
+#endregion
 
+#region configure JWT
+var secretKey = builder.Configuration["AppSettings:SecretKey"];
+var secterKeyByte = Encoding.UTF8.GetBytes(secretKey);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // tự cấp token nên validate = false
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            // có thể sử dụng các dịch vụ cấp token như OAuth2
+
+            // ký vào token
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(secterKeyByte),
+
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
