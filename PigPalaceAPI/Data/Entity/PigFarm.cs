5f418ae7d3423746b3bad2e65af4ef8651cb514f
21522354 <120326395+21522354@users.noreply.github.com>
2024-04-01 15:49:17 +0700

    add Gmail column to Farm

--- PigPalaceAPI/Data/Entity/PigFarm.cs
+++ PigPalaceAPI/Data/Entity/PigFarm.cs
@@ -8,7 +8,7 @@ namespace PigPalaceAPI.Data.Entity
     {
         public int ID { get; set; } 
         public string Name { get; set; }    
-        public string Address { get; set; }
+        public string Gmail { get; set; }
         [Required]
         public string UserName { get; set; }
         [Required]  
--- PigPalaceAPI/Migrations/20240401084744_them cot gmail.Designer.cs
+++ PigPalaceAPI/Migrations/20240401084744_them cot gmail.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PigPalaceAPI.Data;
 
@@ -10,9 +11,11 @@ using PigPalaceAPI.Data;
 namespace PigPalaceAPI.Migrations
 {
     [DbContext(typeof(PigPalaceDBContext))]
-    partial class PigPalaceDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240401084744_them cot gmail")]
+    partial class themcotgmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
